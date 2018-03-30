9faceab06591ee6e500749829df6d88a871c8116
ArcAick <j.rem.s91@live.fr>
2018-03-30 13:58:00 +0200

    final commit

--- pca.py
+++ pca.py
@@ -3,38 +3,59 @@ from keras.models import  Model
 from keras.layers import Dense,Input
 import tensorflow as tf
 from matplotlib import pyplot as plt
+import sklearn.datasets
 
 import numpy as np
 from keras.utils import np_utils
 
-nb_classes = 10
 
-(x_test,y_test), (x_train,y_train) = mnist.load_data()
 
-x_train = x_train.reshape(x_train.shape[0], 28 * 28).astype('float32')
+def pca_for_mnist(data):
+    mean_cols = np.mean(data.T, axis=1)
+    center = data - mean_cols
+    covariance = np.cov(center.T)
+    vals, vects = np.linalg.eig(covariance)
+    idx = np.argsort(vals)[::-1]
+    vects = np.real(vects[idx])
+    vals = np.real(vals[idx])
+    project = vects.T.dot(center.T)
+    return project
 
 def pca(data):
-    #Transpose
-    d = np.transpose(data)
-    #Calcul puissance
-    d_cov = np.cov(d)
-    #idx et vect de chaque covariance
-    i, v = np.linalg.eig(d_cov)
-    #recuperere valeur reel
-    i = np.real(i)
-    v = np.real(v)
+    mean_cols = np.mean(data.T, axis=1)
+    center = data - mean_cols
+    covariance = np.cov(center.T)
+    vals, vects = np.linalg.eig(covariance)
+    project = vects.T.dot(center.T)
+    return project
 
-    #Recup valeur absolu
-    tab = np.argsort(np.abs(i))[::-1]
-    i = i[tab]
-    v = v[tab]
-    return i, v
+
+def run_pca_test():
+    X1, Y1 = sklearn.datasets.make_blobs(n_samples=20000, n_features=2)
+    test = np.array(X1)
+    figure = plt.figure()
+    ax = figure.add_subplot(111)
+    x = np.transpose(pca(test))
+    ax.scatter(x[:,0],x[:,1], c=Y1)
+    plt.show()
+
+def run_pca_mnist():
+    (x_test, y_test), (x_train, y_train) = mnist.load_data()
+    n_rows = 60000
+
+    x_train = x_train.reshape(x_train.shape[0], 28 * 28).astype('float32')
+    x_train  = x_train[:n_rows]
+    mnist_data = pca_for_mnist(x_train)
+    figure = plt.figure()
+    ax = figure.add_subplot(111)
+    x = pca(mnist_data)
+    ax.scatter(x[:, 0], x[:, 1], c=y_train[:n_rows])
+    plt.show()
+
 
 if __name__ == '__main__':
-     pca_i, pca_v = pca(x_train)
-     vects = pca_v[:2]
-     matriciel =  np.dot(x_train,np.transpose(vects))
-     print(matriciel)
+    #run_pca_test()
+    run_pca_mnist()
 
 
 
